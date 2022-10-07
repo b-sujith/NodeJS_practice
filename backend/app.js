33499262ee72e835521f12c17d52e6f712a4c4d2
sujith <71209269+failur-e@users.noreply.github.com>
2022-10-07 13:49:36 +0530

    first basic complete api

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 //express function returns express object that contains http server (app is a conventional name used)
 const app = express()
-
+app.use(express.json())
 //fake data to verify route handlers
 let users=[
     {
@@ -27,19 +27,78 @@ app.get('/getallusers',(request,response)=>{
 })
 
 //creating a route to handle '/getalluser/id'
-app.get('/getuser/:id',(request,response)=>{
+    app.get('/getuser/:id',(request,response)=>{
 
-    let userID = request.params.id; //getting argument of url using params property
+        let userID = request.params.id; //getting argument of url using params property
 
-    //searching for user using id
-    let userObj = users.find(userObj=>userObj.id==userID);
+        //searching for user using id
+        let userObj = users.find(userObj=>userObj.id==userID);
+
+        //if user not found
+        if(userObj==undefined)
+            response.send({message:"USer doesn't exist"});
+        //if user found
+        else
+            response.send({message:"user found",payload:userObj});
+    })
+
+//creating a route to handle "/createuser"
+app.post("/createuser",(request,response)=>{
+    //obtaining user data in json format which is converted to js object
+    userObj = request.body
+    //adding userobj into the users array
+    users.push(userObj)
+    response.send({mesaage:"User created"})
+})
+
+//creating a route to handle "/updateuser"
+app.put("/updateuser",(request,response)=>{
+    //obtaining user data in json format which is converted to js object
+    newuserObj = request.body
+
+    newuserObjId = newuserObj.id    //getting user id to check for the users id in arr
+
+    //searching for the userobj using id
+    let userObj = users.find(userObj=>userObj.id==userID)
 
     //if user not found
     if(userObj==undefined)
-        response.send({message:"USer doesn't exist"});
-    //if user found
-    else
-        response.send({message:"user found",payload:userObj});
+        response.send({message:"user not found"});
+    else{
+
+        //loop to match newuserid with existing id and then updating
+        for(let i=0;i<users.length;i++){
+            if(users[i].id === newuserObjId){
+                users[i] = newuserObj
+                break
+            }
+        }
+    }
+    response.send({message:"user updated"})
+
+})
+
+//creating a route to handle "/deleteuser"
+app.delete("/deleteuser/:id",(request,response)=>{
+    //getting user id from argument to check for the users id in arr
+    let userID = request.params.id
+
+    //searching for the userobj using id
+    let userObj = users.find(userObj=>userObj.id==userID)
+
+    //if user not found
+    if(userObj==undefined)
+        response.send({message:"user not found"});
+    else{
+
+        //loop to match userid and then delete the obj
+        for(let i=0;i<users.length;i++){
+            if(users[i].id==userID){
+                users.splice(i,1);
+            }
+        }
+        response.send({message:"user deleted"});
+    }
 })
 
 //assigning port number to the server obj
