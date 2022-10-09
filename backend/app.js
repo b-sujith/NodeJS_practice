7c1bb0b84b07420f7ee90b3cd7514cf3da60e96f
sujith <71209269+failur-e@users.noreply.github.com>
2022-10-09 17:25:01 +0530

    connected with DB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,32 @@
 //express module returns a function(not object)
-const { response } = require('express')
 const express = require('express')
-
 //express function returns express object that contains http server (app is a conventional name used)
 const app = express()
+
+//import mongodb and retrieve MongoClient
+const mongoClient = require('mongodb').MongoClient
+
+//mongoDB connection url
+const DBurl = "mongodb+srv://sujith:sujith123@cluster0.vceyf.mongodb.net/?retryWrites=true&w=majority"
+
+//connect with MongoDB server-->returns a promise
+mongoClient.connect(DBurl)
+.then((client)=>{
+
+    //get dbObj from client
+    let dbObj = client.db("db1")
+
+    //create collection objs from dbObj
+    let userCollection = dbObj.collection("usercollection");
+    let productCollection = dbObj.collection("productcollection");
+
+    //share collection objects to APIS
+    app.set("userCollection",userCollection);
+    app.set("productCollection",productCollection);    
+
+    console.log("DB Connection is success");
+})
+.catch(err=>console.log("Error in DB connection",err));
 
 //importing userApi,productApi objects 
 const userApp = require('./APIS/userApi')
